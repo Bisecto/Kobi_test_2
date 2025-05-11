3830686f19bda173631d69b6f51055b523edfdc6
precious <bisector0@gmail.com>
2025-05-11 16:52:08 +0100

    removed unused items

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -7,33 +7,6 @@ import 'package:http/http.dart' as http;
 import '../utills/app_utils.dart';
 
 class AppRepository {
-  Future<http.Response> postRequest(
-      Map<String, String> data, String apiUrl) async {
-    var response = await http.post(
-      Uri.parse(apiUrl),
-      body: data,
-    );
-    AppUtils().debuglog(apiUrl+response.statusCode.toString());
-
-    return response;
-  }
-  Future<http.Response> postRequestWithToken(String token,
-      Map<String, dynamic> data, String apiUrl) async {
-    var headers = {
-      'Authorization': 'JWT $token',
-      //'Content-Type': 'application/json'
-    };
-    var response = await http.post(
-      Uri.parse(apiUrl),
-      headers: headers,
-      body: data,
-    );
-    AppUtils().debuglog(apiUrl+response.statusCode.toString());
-    AppUtils().debuglog(1);
-    AppUtils().debuglog(response.body);
-    return response;
-  }
-
 
 
   Future<http.Response> getRequest( String apiUrl) async {
@@ -47,22 +20,5 @@ class AppRepository {
 
     return response;
   }
-  Future<http.Response> getRequestWithToken(String token, String apiUrl) async {
-    AppUtils().debuglog(apiUrl);
-    var headers = {
-      'Authorization': 'JWT $token',
-      'Content-Type': 'application/json'
-    };
-    AppUtils().debuglog(headers);
-    final response = await http.get(
-      Uri.parse(apiUrl),
-      headers: headers,
-      // body: jsonEncode(user),
-    );
-    AppUtils().debuglog(apiUrl+response.statusCode.toString());
-    AppUtils().debuglog(response.statusCode);
-    AppUtils().debuglog(response.statusCode);
-    AppUtils().debuglog(response.body);
-    return response;
-  }
+
 }
